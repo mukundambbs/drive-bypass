26d31b7badced9df6f91f32c41b714fd012d0c0c
PGV <95969441+pachakili@users.noreply.github.com>
2022-02-28 20:16:12 +0530

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -1,3 +1,8 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
 from telegram.ext import CommandHandler
 from bot.helper.drive_utils.gdriveTools import GoogleDriveHelper
 from bot import LOGGER, dispatcher
@@ -26,6 +31,45 @@ def list_drive(update, context):
         emo = sendMessage('😡', context.bot, update)
         sendMessage("𝐃𝐨𝐧'𝐭 𝐮𝐬𝐞 𝐮𝐧𝐧𝐞𝐜𝐞𝐬𝐬𝐚𝐫𝐢𝐥𝐲, 𝐒𝐞𝐧𝐝 𝐚 𝐬𝐞𝐚𝐫𝐜𝐡 𝐤𝐞𝐲 𝐚𝐥𝐨𝐧𝐠 𝐰𝐢𝐭𝐡 𝐜𝐨𝐦𝐦𝐚𝐧𝐝", context.bot, update)
         deleteMessage(context.bot, emo)
+                
+@Client.on_message(filters.command(["gplink"]) & filters.regex(r'https?://[^\s]+'))
+async def reply_shortens(bot, update):
+    message = await update.reply_text(
+        text="`Analysing your link...`",
+        disable_web_page_preview=True,
+        quote=True
+    )
+    url = update.matches[0].group(0)
+    shorten_urls = await gplinks_bypass(url)
+    await message.edit_text(
+        text=shorten_urls,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+    
+def gplinks_bypass(url):
+    client = requests.Session()
+    res = client.get(url)
+    
+    h = { "referer": res.url }
+    res = client.get(url, headers=h)
+    
+    bs4 = BeautifulSoup(res.content, 'lxml')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'content-type': 'application/x-www-form-urlencoded',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    
+    time.sleep(10) # !important
+    
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+    res = client.post(final_url, data=data, headers=h).json()
+
+    return res
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
