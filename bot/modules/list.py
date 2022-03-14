f97176dc9d6aa5b9d367d963910c8c621c29bd21
PGV <95969441+pachakili@users.noreply.github.com>
2022-03-14 23:43:25 +0530

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -31,46 +31,6 @@ def list_drive(update, context):
         emo = sendMessage('😡', context.bot, update)
         sendMessage("𝐃𝐨𝐧'𝐭 𝐮𝐬𝐞 𝐮𝐧𝐧𝐞𝐜𝐞𝐬𝐬𝐚𝐫𝐢𝐥𝐲, 𝐒𝐞𝐧𝐝 𝐚 𝐬𝐞𝐚𝐫𝐜𝐡 𝐤𝐞𝐲 𝐚𝐥𝐨𝐧𝐠 𝐰𝐢𝐭𝐡 𝐜𝐨𝐦𝐦𝐚𝐧𝐝", context.bot, update)
         deleteMessage(context.bot, emo)
-                
-@bot.on_message(filters.command(["droplink"]) & filters.regex(r'https?://[^\s]+'))
-async def reply_shortens(bot, update):
-    message = await update.reply_text(
-        text="`Analysing your link...`",
-        disable_web_page_preview=True,
-        quote=True
-    )
-    url = update.matches[0].group(0)
-    shorten_urls = await droplink_bypass(url)
-    await message.edit_text(
-        text=shorten_urls,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True
-    )
-    
-def droplink_bypass(url):
-    client = requests.Session()
-    res = client.get(url)
-
-    ref = re.findall("action[ ]{0,}=[ ]{0,}['|\"](.*?)['|\"]", res.text)[0]
-
-    h = {'referer': ref}
-    res = client.get(url, headers=h)
-
-    bs4 = BeautifulSoup(res.content, 'lxml')
-    inputs = bs4.find_all('input')
-    data = { input.get('name'): input.get('value') for input in inputs }
-
-    h = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-requested-with': 'XMLHttpRequest'
-    }
-    p = urlparse(url)
-    final_url = f'{p.scheme}://{p.netloc}/links/go'
-
-    time.sleep(3.1)
-    res = client.post(final_url, data=data, headers=h).json()
-
-    return res
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
