50096c8386db10e4c4d1616a1567bb318c6e6c93
PGV <95969441+pachakili@users.noreply.github.com>
2022-03-29 19:34:19 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,6 +4,7 @@ import pytz
 import subprocess
 
 from telegram.ext import CommandHandler
+from telegram import ParseMode
 from bot import dispatcher, updater, botStartTime, IMAGE_URL, OWNER_ID, AUTHORIZED_CHATS
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
