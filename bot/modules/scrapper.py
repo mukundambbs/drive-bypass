690f63ca6394ca9d481babfd122aa2f4ab08b9b8
PGV <95969441+pachakili@users.noreply.github.com>
2022-04-08 20:27:20 +0530

    Update scrapper.py

--- bot/modules/scrapper.py
+++ bot/modules/scrapper.py
@@ -34,6 +34,31 @@ def listmv(c,m):
   for queryss in m.command[1:length]:
     querys += f"{queryss} "
   if querys == "":
+    await m.reply(f'`/listmv [query]`', quote=True)
+  elif querys != "":
+    link = f"https://www.1tamilmv.com/index.php?/search/&q={querys}&search_and_or=and&search_in=titles&sortby=relevancy"
+    txt = await m.reply_text(f"Searching for: {querys} 🔍")
+    driver.get(link)
+    await asyncio.sleep(5)
+    title = driver.title
+    links = driver.find_elements(By.CLASS_NAME, "ipsStreamItem_title")
+    msg = []
+    count = 0
+    
+    for link in links:
+      text = link.text
+      url0 = link.find_element(By.CLASS_NAME, 'ipsType_break')
+      url1 = url0.find_element(By.TAG_NAME, 'a').get_attribute("href")
+      print(url1)
+      count += 1
+      msgs = f"{count}. <a href='{url1}'>{text}</a>\n\n"
+      msg.append(msgs)
+
+    for text in msg[0:20]:
+      texts += text
+    reply = f"<b>{title}</b>\n\n{texts}"
+    await c.send_message(m.chat.id, reply, disable_web_page_preview=True, parse_mode="html")
+    await txt.delete()
 
 
 
